84c81077155d01bc39d007366085a42a3e149f5d
bel-dot <artemonchannel2005@gmail.com>
2025-10-19 02:58:33 +0300

    Change styling of UsersBlock

--- src/components/UserRow.tsx
+++ src/components/UserRow.tsx
@@ -3,7 +3,7 @@ import type User from "../User";
 export default function UserRow(props: propsInterface) {
     return (
         <tr className={props.last ? "" : "border-b border-b-gray-200"}>
-            <td className="py-3 w-[10%] text-center">{props.user.id}</td>
+            <td className="py-3 w-[10%] text-center text-gray-400">{props.user.id}</td>
             <td className="w-[30%] pl-3 text-left">{props.user.name}</td>
             <td className="w-[20%] text-center">{props.user.birth}</td>
             <td className="w-[20%] text-center">{props.user.email}</td>
--- src/components/UsersBlock.tsx
+++ src/components/UsersBlock.tsx
@@ -7,7 +7,7 @@ export default function UsersBlock(props: propsInterface) {
             <table className="w-full table-fixed">
                 <thead className="border-b border-b-gray-300">
                     <tr>
-                        <th className="py-3 w-[5%]">#</th>
+                        <th className="py-3 w-[5%] text-gray-400">#</th>
                         <th className="w-[35%] pl-3 text-start">Name</th>
                         <th className="w-[20%]">Date of Birth</th>
                         <th className="w-[20%]">Email</th>
